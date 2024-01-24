39f079b2816ab4c4fb958f6933e2151e71b9e724
LaikaFusion <newrad4@gmail.com>
2024-01-23 17:56:53 -0800

    Initial stuff, if you have to revert this far good luck

---
+++ src/Game/Gameboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Gameboard() {
+  return (
+    <div>Gameboard</div>
+  )
+}
+
+Gameboard.propTypes = {}
+
+export default Gameboard
