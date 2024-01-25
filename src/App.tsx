d6a07660052381e1040593223b4eabb5b7c3a4fa
LaikaFusion <newrad4@gmail.com>
2024-01-24 18:20:41 -0800

    number insertion works

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,70 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
+interface currentCordsObj {
+  row: number;
+  col: number;
+}
+
 function App() {
+  // const initialGameBoard = Array(9).fill(Array(9).fill(0));
   const initialGameBoard = Array(9).fill(Array(9).fill(0));
   const [gameBoard, setGameboard] = useState(initialGameBoard);
+  const [currentSelectedDigit, setDigit] = useState(0);
+
+  //for keyboard input
+  const numberSelectViaKey = ({ key }: KeyboardEvent) => {
+    const lastPress = Number(key);
+    // we want 1-9, no 0
+    if (lastPress) {
+      setDigit(lastPress);
+    }
+  };
+
+  const changeGameboardValue = (currentCords: currentCordsObj) => {
+    if (currentSelectedDigit) {
+      const newGameBoard = [...gameBoard];
+      const changedCol = [...newGameBoard[currentCords.col]];
+      changedCol[currentCords.row] = currentSelectedDigit;
+      newGameBoard[currentCords.col] = changedCol;
+
+      setGameboard(newGameBoard);
+    }
+  };
+
+  // const updateGrid = (numberToChange, cordnates) =>
+  useEffect(() => {
+    document.addEventListener("keydown", numberSelectViaKey, true);
+
+    return () => {
+      document.removeEventListener("keydown", numberSelectViaKey, true);
+    };
+  }, [currentSelectedDigit]);
 
   return (
     <>
+      <div> {currentSelectedDigit}</div>
+
       <div className="gameBoard">
-        {gameBoard.map((row) => {
+        {gameBoard.map((rowOfBoard: Array<number>, curCol) => {
           return (
-            <div>
-              {row.map((numberToFill: number) => {
-                return <div className="gameCell">{numberToFill}</div>;
+            <div key={curCol}>
+              {rowOfBoard.map((numberToFill: number, curRow) => {
+                const currentCords = {
+                  row: curRow,
+                  col: curCol,
+                };
+
+                return (
+                  <div
+                    //there has to be a better way to do this.
+                    key={curRow + "" + curCol}
+                    className="gameCell"
+                    onClick={() => changeGameboardValue(currentCords)}
+                  >
+                    {numberToFill ? numberToFill : ""}
+                  </div>
+                );
               })}
             </div>
           );
