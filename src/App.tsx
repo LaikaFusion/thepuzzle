de9d7f28e2ce5633a5563c21a9a2aff02986f1e7
LaikaFusion <newrad4@gmail.com>
2024-01-28 12:18:12 -0800

    checking works

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import { element } from "prop-types";
 
 interface currentCordsObj {
   row: number;
@@ -7,10 +8,174 @@ interface currentCordsObj {
 }
 
 function App() {
-  // const initialGameBoard = Array(9).fill(Array(9).fill(0));
+  // note the gameboard is an array of columns, not of rows
   const initialGameBoard = Array(9).fill(Array(9).fill(0));
   const [gameBoard, setGameboard] = useState(initialGameBoard);
   const [currentSelectedDigit, setDigit] = useState(0);
+  const [invalidBoard, setInvalidBoard] = useState(false);
+
+  const noDupeNumbers = (row: Array<number>) => {
+    const dupes: number[] = [];
+
+    row.forEach((element: number, index: number) => {
+      if (index !== row.lastIndexOf(element) && element !== 0) {
+        dupes.push(element);
+      }
+    });
+    //making an array here to eventually do highlighting but that's not a concern yet
+    return [...new Set(dupes)];
+  };
+
+  const boxCheck = (board: Array<Array<number>>) => {
+    //it was late and I didn't feel like debugging nested loops
+    const rowOne = [
+      board[0][0],
+      board[0][1],
+      board[0][2],
+      board[1][0],
+      board[1][1],
+      board[1][2],
+      board[2][0],
+      board[2][1],
+      board[2][2],
+    ];
+    const rowTwo = [
+      board[0][3],
+      board[0][4],
+      board[0][5],
+      board[1][3],
+      board[1][4],
+      board[1][5],
+      board[2][3],
+      board[2][4],
+      board[2][5],
+    ];
+    const rowThree = [
+      board[0][6],
+      board[0][7],
+      board[0][8],
+      board[1][6],
+      board[1][7],
+      board[1][8],
+      board[2][6],
+      board[2][7],
+      board[2][8],
+    ];
+    const rowFour = [
+      board[3][0],
+      board[3][1],
+      board[3][2],
+      board[4][0],
+      board[4][1],
+      board[4][2],
+      board[5][0],
+      board[5][1],
+      board[5][2],
+    ];
+    const rowFive = [
+      board[3][3],
+      board[3][4],
+      board[3][5],
+      board[4][3],
+      board[4][4],
+      board[4][5],
+      board[5][3],
+      board[5][4],
+      board[5][5],
+    ];
+    const rowSix = [
+      board[3][6],
+      board[3][7],
+      board[3][8],
+      board[4][6],
+      board[4][7],
+      board[4][8],
+      board[5][6],
+      board[5][7],
+      board[5][8],
+    ];
+    const rowSeven = [
+      board[6][0],
+      board[6][1],
+      board[6][2],
+      board[7][0],
+      board[7][1],
+      board[7][2],
+      board[8][0],
+      board[8][1],
+      board[8][2],
+    ];
+    const rowEight = [
+      board[6][3],
+      board[6][4],
+      board[6][5],
+      board[7][3],
+      board[7][4],
+      board[7][5],
+      board[8][3],
+      board[8][4],
+      board[8][5],
+    ];
+    const rowNine = [
+      board[6][6],
+      board[6][7],
+      board[6][8],
+      board[7][6],
+      board[7][7],
+      board[7][8],
+      board[8][6],
+      board[8][7],
+      board[8][8],
+    ];
+    const boxedBoard = [
+      rowOne,
+      rowTwo,
+      rowThree,
+      rowFour,
+      rowFive,
+      rowSix,
+      rowSeven,
+      rowEight,
+      rowNine,
+    ];
+    let isProblem = false;
+    boxedBoard.forEach((element: Array<number>) => {
+      const results = noDupeNumbers(element);
+      if (results.length > 0) {
+        isProblem = true;
+      }
+    });
+    return isProblem;
+  };
+
+  const rowCheck = (board: Array<Array<number>>) => {
+    const rotatedBoard: Array<Array<number>> = [];
+    let isProblem = false;
+    for (let index = 0; index < board.length; index++) {
+      const rowArray: Array<number> = [];
+      board.forEach((column) => {
+        rowArray.push(column[index]);
+      });
+      rotatedBoard.push(rowArray);
+    }
+    rotatedBoard.forEach((element: Array<number>) => {
+      const results = noDupeNumbers(element);
+      if (results.length > 0) {
+        isProblem = true;
+      }
+    });
+    return isProblem;
+  };
+  const colCheck = (board: Array<Array<number>>) => {
+    board.forEach((element: Array<number>) => {
+      const results = noDupeNumbers(element);
+      if (results.length > 0) {
+        console.log("bad column");
+        return true;
+      }
+    });
+    return false;
+  };
 
   //for keyboard input
   const numberSelectViaKey = ({ key }: KeyboardEvent) => {
@@ -21,6 +186,7 @@ function App() {
     }
   };
 
+  //for updating the master game value, this is gonna get big
   const changeGameboardValue = (currentCords: currentCordsObj) => {
     if (currentSelectedDigit) {
       const newGameBoard = [...gameBoard];
@@ -28,6 +194,15 @@ function App() {
       changedCol[currentCords.row] = currentSelectedDigit;
       newGameBoard[currentCords.col] = changedCol;
 
+      if (
+        colCheck(newGameBoard) ||
+        rowCheck(newGameBoard) ||
+        boxCheck(newGameBoard)
+      ) {
+        setInvalidBoard(true);
+      } else {
+        setInvalidBoard(false);
+      }
       setGameboard(newGameBoard);
     }
   };
@@ -69,6 +244,7 @@ function App() {
             </div>
           );
         })}
+        <div>{invalidBoard ? "Wrong" : "All Right"}</div>
       </div>
     </>
   );
